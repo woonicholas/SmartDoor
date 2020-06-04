e1816b9c30edfb785d1f5633aec0957c863eaba2
Ivan Huang <huangiw@uci.edu>
2020-06-04 15:39:04 -0700

    spotipy changes

--- edge/music_client.py
+++ edge/music_client.py
@@ -13,11 +13,35 @@ import sys
 sys.path.insert(0, '..')
 from constants import *
 
+from dotenv import load_dotenv
+
+from pathlib import Path
+# env_path = Path("/home/pi/Desktop/SmartDoor/.env")
+# load_dotenv(dotenv_path=env_path)
+
+import os 
+import spotipy
+import spotipy.util as util
+import time
+
+username="ivanhuang77@gmail.com"
+raspotify_id = os.getenv("raspotify_device_id")
+spotify_client_id = os.getenv("spotify_client_id")
+spotify_client_secret = os.getenv("spotify_client_secret")
+
+# scope = 'user-library-read'
+scope = "user-read-playback-state,user-modify-playback-state"
 
 
 def music_client_program():
 
-  pygame.mixer.init()
+  # pygame.mixer.init()
+
+  token = util.prompt_for_user_token( username, 
+                                    scope, 
+                                    client_id="6cfd44e621824a4696b47f22e0986704",
+                                    client_secret="1ca2d9650136456bba19555747456547",
+                                    redirect_uri='http://localhost:8000/'  )
   
   context = zmq.Context()
   socket = context.socket(zmq.SUB)
@@ -27,38 +51,45 @@ def music_client_program():
   print('Speaker connected to (' + str((HOST, OUT_PORT)) + ')')
   print('Waiting to play music..')
 
-  names = {
-    'scissors': 'Ivan',
-    'banana' : 'Preston',
-    'fork': 'Nick',
-    'person': 'Henry'
-  }
-
   try:
     while True:
       song_rec = socket.recv().decode()
+      print(song_rec)
       #speaker banana enter ymca.mp3 2/4/20
       #speaker banana exit 2/4/20
-      print(song_rec)
+
       splitted = song_rec.split()
       name = splitted[1]
       action = splitted[2]
       if(action == 'enter'):
         song = splitted[3]
         datetime = splitted[4]
-        pygame.mixer.music.load('/songs/' + song)
-        pygame.mixer.music.set_volume(1.0)
-        pygame.mixer.music.play()
+        # pygame.mixer.music.load('/songs/' + song)
+        # pygame.mixer.music.set_volume(1.0)
+        # pygame.mixer.music.play()
 
-        time.sleep(10)
-        pygame.mixer.music.stop()
+        # time.sleep(10)
+        # pygame.mixer.music.stop()
+        if token:
+            sp = spotipy.Spotify(auth=token)
+            
+            results = sp.search(q=song, limit=3)
+            
+            firstResult = results['tracks']['items'][0]
+            print('Spotify search result: ' + firstResult['name'])
+            print(firstResult['name'] + ' is now playing!')
 
-        # p = multiprocessing.Process(target=playsound, args=('songs/'+song,))
-        # p.start()
-        # input('Press Enter to Stop Playing')
-        # p.terminate()
-        
-        prediction = requests.get('http://35.236.46.222:8080/%s/%s' % (names[name], datetime), 
+            ## Change playback on the active device
+            sp.start_playback(device_id='43a0a1a8680013a0974a9aaf5eac96700e2007f4', uris=['spotify:track:' + firstResult['id']])    
+            # sp.volume(100, device_id='43a0a1a8680013a0974a9aaf5eac96700e2007f4')
+            
+            time.sleep(10)
+            sp.pause_playback(device_id='43a0a1a8680013a0974a9aaf5eac96700e2007f4')
+        else:
+            print("Can't get token for", username)        
+
+
+        prediction = requests.get('http://35.236.46.222:8080/leave/%s/%s' % (name, datetime), 
                 auth=('smartdoorcool',
                       'smartdoorpass'))
 
--- edge/spotipy_test.py
+++ edge/spotipy_test.py
@@ -1,12 +1,13 @@
 from dotenv import load_dotenv
 
 from pathlib import Path
-env_path = Path("/home/pi/Desktop/SmartDoor/.env")
-load_dotenv(dotenv_path=env_path)
+# env_path = Path("/home/pi/Desktop/SmartDoor/.env")
+# load_dotenv(dotenv_path=env_path)
 
 import os 
 import spotipy
 import spotipy.util as util
+import time
 
 username="ivanhuang77@gmail.com"
 raspotify_id = os.getenv("raspotify_device_id")
@@ -21,13 +22,13 @@ print(spotify_client_id)
 
 token = util.prompt_for_user_token( username, 
                                     scope, 
-                                    client_id=spotify_client_id,
-                                    client_secret=spotify_client_secret,
+                                    client_id="6cfd44e621824a4696b47f22e0986704",
+                                    client_secret="1ca2d9650136456bba19555747456547",
                                     redirect_uri='http://localhost:8000/'  )
 if token:
     sp = spotipy.Spotify(auth=token)
     
-    results = sp.search(q='without you avicii', limit=3)
+    results = sp.search(q='without you avicii', limit=10)
     
     ## printing track name and ids
     # for idx, track in enumerate(results['tracks']['items']):
@@ -37,9 +38,11 @@ if token:
     print(firstResult['name'])
 
     ## Change playback on the active device
-    sp.start_playback(device_id=raspotify_id, uris=['spotify:track:' + firstResult['id']])    
-    sp.volume(80)
+    sp.start_playback(device_id='43a0a1a8680013a0974a9aaf5eac96700e2007f4', uris=['spotify:track:' + firstResult['id']])    
+    # sp.volume(100, device_id='43a0a1a8680013a0974a9aaf5eac96700e2007f4')
     
+    time.sleep(10)
+    sp.pause_playback(device_id='43a0a1a8680013a0974a9aaf5eac96700e2007f4')
     ## Finds devices with spotify open
     # res = sp.devices()
     # print(res)
