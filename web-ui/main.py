f1b474356f052ee0b0d84cfbbe7d14bfe98103cf
Nicholas Woo <nickj_woo@yahoo.com>
2020-06-05 16:22:15 -0700

    get ui to show predictions

--- web-ui/main.py
+++ web-ui/main.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template, request, jsonify, make_response
 from flask_cors import CORS
 import json
 import ui_client
+import requests
+from datetime import datetime
 
 app = Flask(__name__)
 CORS(app)
+
+def get_time_for_api():
+  now = datetime.now()
+  if(now.weekday() < 5):
+    return '%d/%d/%d' % (8, 30, now.weekday())
+  else:
+    return '%d/%d/%d' % (now.hour, now.minute, now.weekday())
 
 @app.route('/')
 def home_page():
@@ -22,7 +31,26 @@ def employee_page(id):
       if i["id"] == id:
         data = i
         break
-  return render_template("employee.html", employee=data)
+  now = datetime.now()
+  enter_time = requests.get('http://35.236.46.222:8080/enter/%s/%s' % (data['name'], get_time_for_api()), 
+        auth=('smartdoorcool',
+              'smartdoorpass'))
+  enter_prediction = json.loads(enter_time.text)
+  leave_time = requests.get('http://35.236.46.222:8080/leave/%s/%s/%s/%s' % (data['name'], \
+        enter_prediction['predicted_enter'][0], enter_prediction['predicted_enter'][1], now.weekday()),
+        auth=('smartdoorcool',
+              'smartdoorpass'))
+  leave_prediction = json.loads(leave_time.text)
+
+  predictions = dict()
+  predictions['predicted_enter'] = datetime(now.year, now.month, now.day, enter_prediction['predicted_enter'][0], enter_prediction['predicted_enter'][1]).isoformat()
+  predictions['predicted_leave'] = datetime(now.year, now.month, now.day, leave_prediction['leave'][0], leave_prediction['leave'][1]).isoformat()
+  predictions['day_start'] = datetime(now.year,now.month, now.day, 0).isoformat()
+  predictions['day_end'] = datetime(now.year,now.month, now.day, 23).isoformat()
+
+  print(predictions)
+
+  return render_template("employee.html", employee=data, predictions = predictions)
 
 @app.route('/select-song', methods = ['PUT'])
 def select_song():
